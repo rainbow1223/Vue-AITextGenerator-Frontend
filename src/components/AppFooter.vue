2595f6f66868eca7af6823c308d405251195d631
StrongJack123 <greatwisdom1223@gmail.com>
2022-11-24 18:13:24 +0900

    add app components

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-footer app id="footer">
+    <v-spacer></v-spacer>
+    <div>Made with Vuetify by <a href="http://thomas-lamson.com/" target="_blank">Thomas Lamson</a>, All rights reserved &copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
+</template>
+
+<style>
+#footer {
+    font-size: 14px;
+    background-color: #d6d6d6;
+    height: 30px;
+    max-height: 30px;
+    width: 100vw;
+    max-width: 100vw;
+}
+</style>
